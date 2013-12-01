7646870acbda47ac7077d2bca746b4f77599a284
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-12-01 11:07:51 +0100

    Update bootstrap.sh to accept revision as argument.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+REV=${1:-1.0}
+
+echo '* Bootstrap for revision '${REV}
 
 echo '* changing MYGST'
-sed -i '/MYGST=/{s_$HOME/gst_'$PWD'_;}' 1.0/gstreamer/scripts/gst-uninstalled
+sed -i '/MYGST=/{s_$HOME/gst_'$PWD'_;}' ${REV}/gstreamer/scripts/gst-uninstalled || exit 1
 
 echo '* simlink'
-ln -s 1.0/gstreamer/scripts/gst-uninstalled gst-1.0
+ln -s ${REV}/gstreamer/scripts/gst-uninstalled gst-${REV} || exit 1
